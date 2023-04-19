1c00922fbe451daf31386d711acbd6916562f386
AleksandrButakov <anbngm@gmail.com>
2023-04-19 23:34:57 +0500

    Changed code

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -37,6 +37,30 @@ public class Attach {
         );
     }
 
+
+
+//    @Attachment(value = "{attachName}", type = "text/plain")
+//    public static String attachAsText(String attachName, String message) {
+//        return message;
+//    }
+//
+//    @Attachment(value = "Page source", type = "text/plain")
+//    public static byte[] pageSource() {
+//        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+//    }
+//
+//    @Attachment(value = "{attachName}", type = "image/png")
+//    public static byte[] screenshotAs(String attachName) {
+//        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+//    }
+//
+//    public static void browserConsoleLogs() {
+//        attachAsText(
+//                "Browser console logs",
+//                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+//        );
+//    }
+
 //    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
 //    public static String addVideo() {
 //        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
@@ -56,8 +80,8 @@ public class Attach {
 //        return null;
 //    }
 
-//    public static String getSessionId() {
-//        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
-//    }
+    public static String getSessionId() {
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
 
 }
